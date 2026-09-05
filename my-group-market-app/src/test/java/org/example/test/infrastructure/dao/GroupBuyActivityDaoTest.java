5f86e8903f5a35cd3a8d477673e76d8b41cd615c
NULLICN <121741693+NULLICN@users.noreply.github.com>
2026-09-05 10:51:48 +0800

    feat: 第一节测试

--- my-group-market-app/src/test/java/org/example/test/infrastructure/dao/GroupBuyActivityDaoTest.java
+++ my-group-market-app/src/test/java/org/example/test/infrastructure/dao/GroupBuyActivityDaoTest.java
@@ -1,13 +1,18 @@
-package org.example.test;
+package org.example.test.infrastructure.dao;
 
-import com.alibaba.fastjson2.JSON;
+
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
+import org.example.infrastructure.dao.IGroupBuyActivityDao;
+import org.example.infrastructure.dao.po.GroupBuyActivity;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Slf4j
 @RunWith(SpringRunner.class)
@@ -24,4 +29,3 @@ public class GroupBuyActivityDaoTest {
     }
 
 }
-
---
+++ my-group-market-infrastructure/src/main/java/org/example/infrastructure/dao/IGroupBuyDiscountDao.java
@@ -0,0 +1,19 @@
+package org.example.infrastructure.dao;
+
+
+import org.apache.ibatis.annotations.Mapper;
+import org.example.infrastructure.dao.po.GroupBuyDiscount;
+
+import java.util.List;
+
+/**
+ * @author Fuzhengwei bugstack.cn @小傅哥
+ * @description 折扣配置Dao
+ * @create 2024-12-07 10:10
+ */
+@Mapper
+public interface IGroupBuyDiscountDao {
+
+    List<GroupBuyDiscount> queryGroupBuyDiscountList();
+
+}
