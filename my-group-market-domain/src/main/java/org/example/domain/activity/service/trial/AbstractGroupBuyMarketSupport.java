252785f3021fdae1a8cf7a6c983e01abe60a5a17
NULLICN <121741693+NULLICN@users.noreply.github.com>
2026-09-07 11:30:29 +0800

    feat: 2-3 多线程异步数据加载

--- my-group-market-domain/src/main/java/org/example/domain/activity/service/trial/AbstractGroupBuyMarketSupport.java
+++ my-group-market-domain/src/main/java/org/example/domain/activity/service/trial/AbstractGroupBuyMarketSupport.java
@@ -3,7 +3,11 @@ package org.example.domain.activity.service.trial;
 import org.example.domain.activity.model.entity.MarketProductEntity;
 import org.example.domain.activity.model.entity.TrialBalanceEntity;
 import org.example.domain.activity.service.trial.factory.DefaultActivityStrategyFactory;
+import org.example.types.design.framework.tree.AbstractMultiThreadStrategyRouter;
 import org.example.types.design.framework.tree.AbstractStrategyRouter;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
 
 /**
  * 功能服务支撑类 用于明确指定入参与出参的定义
@@ -12,8 +16,12 @@ import org.example.types.design.framework.tree.AbstractStrategyRouter;
  * @param <TrialBalanceEntity>
  */
 public abstract class AbstractGroupBuyMarketSupport<MarketProductEntity, DynamicContext, TrialBalanceEntity>
-        extends AbstractStrategyRouter<org.example.domain.activity.model.entity.MarketProductEntity,
-        DefaultActivityStrategyFactory.DynamicContext,
-        org.example.domain.activity.model.entity.TrialBalanceEntity> {
+        extends AbstractMultiThreadStrategyRouter<org.example.domain.activity.model.entity.MarketProductEntity,
+                DefaultActivityStrategyFactory.DynamicContext,
+                org.example.domain.activity.model.entity.TrialBalanceEntity> {
 
+    @Override
+    protected void multiThread(org.example.domain.activity.model.entity.MarketProductEntity requestParameter, DefaultActivityStrategyFactory.DynamicContext dynamicContext) throws ExecutionException, InterruptedException, TimeoutException {
+
+    }
 }
